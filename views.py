0f9972730000e8efa8e78c4b35b0d20a6116b2a1
Rawda <rawda95.r@gmail.com>
2018-01-13 02:16:36 +0200

    add templates 	new file:   itemsCatalog.db 	new file:   models.pyc 	new file:   templates/EditCategory.html 	new file:   templates/EditItem.html 	new file:   templates/NewCategory.html 	new file:   templates/NewItem.html 	new file:   templates/ShowCategoryItems.html 	new file:   templates/ShowItem.html 	new file:   templates/deleteCategory.html 	new file:   templates/deleteItem.html 	new file:   templates/login.html 	new file:   templates/main.html 	modified:   views.py

--- views.py
+++ views.py
@@ -1,8 +1,9 @@
-from flask import Flask ,request , url_for , jsonify , render_template ,redirect
-from models import Base , Categorie , Item
+from flask import Flask, render_template ,request, redirect ,url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base , Category ,Item ,User
 
+from models import Base
 
 engine = create_engine('sqlite:///itemsCatalog.db')
 Base.metadata.bind = engine
@@ -10,59 +11,180 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
 app = Flask(__name__)
+
 
 @app.route('/login')
 def login():
-    return 'login '
+    return render_template('login.html')
 
 
 @app.route('/')
 @app.route('/catalog')
 def main():
-
-    return 'main page'
+    categories = session.query(Category).all()
+    lastItems = session.query(Item).order_by(Item.id.desc()).limit(10).all()
+    return render_template('main.html', categories=categories, lastItems=lastItems)
 
 
 @app.route('/category/new', methods=['POST', 'GET'])
-def NewCategorie():
-    return 'new category'
+def NewCategory():
+    if request.method == 'POST':
+        name = request.form['name']
+        category = session.query(Category).filter_by(name=name).first()
+        if category is None:
+            category = Category(name=name)
+            session.add(category)
+            session.commit()
+            return redirect(url_for('main'))
+        else:
+            return render_template('NewCategory.html', error='the name is already existing')
+    else:
+        return render_template('NewCategory.html')
 
 
 @app.route('/category/<string:category_name>/edit', methods=['POST', 'GET'])
 def EditCategory(category_name):
-    return 'edit category id=%s' % category_name
+    category = session.query(Category).filter_by(name=category_name).first()
+    if category is not None:
+        if request.method == 'POST':
+            name = request.form['name']
+            ifcategory = session.query(Category).filter_by(name=name).first()
+            if ifcategory is None:
+                category.name = name
+                session.add(category)
+                session.commit()
+                return redirect(url_for('main'))
+            else:
+                return render_template('EditCategory.html',
+                                       category=category,
+                                       error='the name is already existing')
+        else:
+            return render_template('EditCategory.html', category=category)
+
+    else:
+        return render_template('EditCategory.html', notexisting='no category with that name ')
 
 
 @app.route('/category/<string:category_name>/delete', methods=['POST', "GET"])
-def delteCatalog(category_name):
-    return 'delete catalog id=%s' % category_name
+def deleteCategory(category_name):
+    category = session.query(Category).filter_by(name=category_name).first()
+    if category is not None:
+        if request.method == 'POST':
+            session.delete(category)
+            session.commit()
+            return redirect(url_for('main'))
+        else:
+            return render_template('deleteCategory.html', category=category)
+    else:
+        return render_template('deleteCategory.html', notexisting='no category with that name ')
 
 
-@app.route('/category/<string:catalog_name>/')
-@app.route('/catalog/<string:catalog_name>/items')
-def ShowCatalogItem(catalog_name):
-    return 'show catalog item  %s' % catalog_name
+@app.route('/category/<string:category_name>/')
+@app.route('/category/<string:category_name>/items')
+def ShowCategoryItems(category_name):
+    category = session.query(Category).filter_by(name=category_name).first()
+    if category is not None:
+        items = session.query(Item).filter_by(category_id=category.id).all()
+        return render_template('ShowCategoryItems.html', items=items, category=category)
+    else:
+        return render_template('ShowCategoryItems.html', notexisting='no category with that name')
 
 
-@app.route('/catalog/<string:catalog_name>/items/new')
-def NewItem(catalog_name):
-    return 'making new item in catalog %s' % catalog_name
+@app.route('/category/<string:category_name>/items/new',methods=['POST','GET'])
+def NewItem(category_name):
+    category = session.query(Category).filter_by(name=category_name).first()
+    if request.method == 'POST':
+        if category is not None:
+            name = request.form['name']
+            description = request.form['description']
+            item = Item(name=name, description=description,category_id= category.id)
+            session.add(item)
+            session.commit()
+            return redirect(url_for('ShowCategoryItems', category_name=category.name))
+        else:
+            return render_template('NewItem.html', error='no category with that name %s'%category_name)
+    else:
+        return render_template('NewItem.html', category=category)
 
-@app.route('/catalog/<string:catalog_name>/items/<string:item_name>')
-def ShowItem(catalog_name, item_name):
-    return 'show item %s in catalog %s' % item_name, catalog_name
 
-@app.route('/catalog/<string:catalog_name>/items/<string:item_name>/edit')
-def EditItem(catalog_name,item_name):
-    return 'edit item id =%s in catalog %s'% item_name,catalog_name
+@app.route('/category/<string:category_name>/items/<string:item_name>')
+def ShowItem(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).first()
+    if category is not None:
+        item = session.query(Item).filter_by(name=item_name, category_id=category.id).first()
+        if item is not None:
+            return render_template('ShowItem.html', item=item, category=category)
+        else:
+            return render_template('ShowItem.html',
+                                   error='no item with that name %s in that category %s'
+                                         % (item_name, category_name))
+    else:
+            return render_template('ShowItem.html',
+                                   error='no category with that name %s '
+                                         % category_name)
 
 
-@app.route('/catalog/<string:catalog_name>/items/<string:item_namr>/delete')
-def deleteItem(catalog_name ,item_name):
-    return 'delete item id =%s in catalog %s' %item_name , catalog_name
 
+@app.route('/category/<string:category_name>/items/<string:item_name>/edit',methods=['POST','GET'])
+def EditItem(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).first()
+    if category is not None:
+        item = session.query(Item).filter_by(name=item_name, category_id=category.id).first()
+        if item is not None:
+            if request.method == 'POST':
+                if request.form['name'] and request.form.get('name','') != item.name :
+                    requestName = request.form['name']
+                    ifitem = session.query(Item).filter_by(name=requestName,
+                                                           category_id=category.id).first()
+                    if ifitem is None:
+                        item.name = requestName
+                    else:
+                        return render_template('EditItem.html',
+                                               category=category,
+                                               item=item,
+                                               error='tha name is already existing')
+                if request.form['description']:
+                    item.description = request.form['description']
+                session.add(item)
+                session.commit()
+                return redirect(url_for('ShowItem',
+                                        category_name=category_name,
+                                        item_name=item.name))
+            else:
+                return render_template('EditItem.html', item=item, category=category)
+        else:
+            return render_template('EditItem.html',
+                                   error='no item with that name %s in that category %s'
+                                         % (item_name, category_name))
+    else:
+        return render_template('EditItem.html',
+                                   error='no category with that name %s '
+                                         % category_name)
+
+
+@app.route('/category/<string:category_name>/items/<string:item_name>/delete',methods=['POST','GET'])
+def deleteItem(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).first()
+    if category is not None:
+        item = session.query(Item).filter_by(name=item_name, category_id=category.id).first()
+        if item is not None:
+            if request.method == 'POST':
+                session.delete(item)
+                session.commit()
+                return redirect(url_for('ShowCategoryItems',category_name =category.name))
+            else:
+                return render_template('deleteItem.html',
+                                       item=item,
+                                       category=category)
+        else:
+            return render_template('deleteItem.html',
+                                   error='no item with that name %s in that category %s'
+                                         % (item_name, category_name))
+    else:
+        return render_template('deleteItem.html',
+                               error='no category with that name %s '
+                                     % category_name)
 
 
 if __name__ == '__main__':
