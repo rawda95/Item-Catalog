a9d3eab0cf53b82052e453bd663872c6e81b33e9
Rawda <rawda95.r@gmail.com>
2018-04-09 02:16:48 +0200

    cahnge ip

--- views.py
+++ views.py
@@ -568,7 +568,7 @@ if __name__ == '__main__':
     app.debug = True
     app.secret_key = 'super_secret_key'
     port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
-    app.run(host='52.29.29.55', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 
